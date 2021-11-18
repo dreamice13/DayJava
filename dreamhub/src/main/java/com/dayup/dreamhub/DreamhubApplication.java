e64e2d4c982266252532143e1fd24bfdc0b96dfc
Dreamice <dreamice13@foxmail.com>
2021-11-18 18:14:13 +0800

    dreamhub init

---
+++ dreamhub/src/main/java/com/dayup/dreamhub/DreamhubApplication.java
@@ -0,0 +1,13 @@
+package com.dayup.dreamhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamhubApplication.class, args);
+    }
+
+}
---
+++ dreamhub/src/main/java/com/dayup/dreamhub/config/MybatisPlusConfig.java
@@ -0,0 +1,11 @@
+package com.dayup.dreamhub.config;
+
+/**
+ * @author ：mBing
+ * @date ：Created in 2021/11/18 18:11
+ * @description：MybatisPlus配置类
+ * @modified By：
+ * @version: 1.0$
+ */
+public class MybatisPlusConfig {
+}
